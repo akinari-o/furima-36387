ad1f034b598d46b45c6fc56952afb131846d7d3e
akinari-o <87591512+akinari-o@users.noreply.github.com>
2021-08-16 19:34:34 +0900

    routesの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
+  resources :items, only: [:new, :create]
+
+  
+  resources :categories, only: [:new, :create]
+  
+  resources :conditions, only: [:new, :create]
+  
+  resources :day_to_ships, only: [:new, :create]
+  
+  resources :delivery_fees, only: [:new, :create]
+
+  resources :prefectures, only: [:new, :create]
+
 end
