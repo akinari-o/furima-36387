6474532948da7f696577395ddea63462f5b828fd
akinari-o <87591512+akinari-o@users.noreply.github.com>
2021-08-17 11:02:01 +0900

    prefectureモデルの編集

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -21,7 +21,4 @@ class Prefecture < ActiveHash::Base
   include ActiveHash::Associations
   has_many :items
 
-  #ジャンルの選択が「--」の時は保存できないようにする
-  #validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-
 end
