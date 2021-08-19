37d6584a577cbb1609fa5fd44aad7e02dac86741
akinari-o <87591512+akinari-o@users.noreply.github.com>
2021-08-19 10:56:09 +0900

    itemコントローラーにおけるedit、updateの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,21 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+  def update
+      @item = Item.find(params[:id])
+      
+      if @item.update(item_params)
+        redirect_to item_path
+      else
+        render :edit
+      end
+  end
   private
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :price, :category_id, :condition_id, :day_to_ship_id, :delivery_fee_id, :prefecture_id).merge(user_id: current_user.id)
