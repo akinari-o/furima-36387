8b9d029f6c396a5cfb487df999749ea62fc52a87
akinari-o <87591512+akinari-o@users.noreply.github.com>
2021-08-26 13:46:46 +0900

    routesの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-  resources :product_buys
+  resources :product_buys, only: [:index, :create]
   end
-
-  
-  
-
 end
